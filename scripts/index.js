76f159731b7136f5ab7d3e182c5465fddec2784b
Даша <dpodkina@mail.ru>
2024-12-31 18:36:53 +0400

    Исправление ошибок после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,34 @@
-//Добавили карточки на страницу
-
-// Объявили переменную, где будут храниться карточки
-const placesList = document.querySelector(".places__list");
-
-const cardInfo = initialCards.map(function (item) {
-  return {
-    name: item.name,
-    link: item.link,
-  };
-});
-
 function render() {
-  cardInfo.forEach(renderCard);
-}
-
-function renderCard({ name, link }) {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate
-    .querySelector(".places__item")
-    .cloneNode(true);
-  cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
+  const placesList = document.querySelector(".places__list");
+  initialCards.forEach(renderCard);
   placesList.append(cardElement);
 }
 
-render();
-
-//Удаление карточек
-
-const content = document.querySelector(".content");
-
-const deleteCard = content.querySelector(".card__delete-button");
-
-deleteCard.addEventListener("click", function () {
+//Объявляем функцию, которую нужно вызывать при нажатии на кнопку(удаление)
+const callback = () => {
   const cards = document.querySelectorAll(".places__list");
   for (let i = 0; i < cards.length; i++) {
     cards[i].remove();
   }
-});
+};
+
+function renderCard({ name, link, alt }) {
+  const content = document.querySelector(".content");
+  const deleteCard = content.querySelector(".card__delete-button");
+  const cardTemplate = document.querySelector("#card-template").content;
+  //1 шаг- Скопировали шаблон
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  //2 шаг - установили значения вложенных элементов
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = (link, (alt = name));
+  return (
+    (name = ""),
+    ((link = ""), (alt = "")),
+    //3 шаг - назначили обработчик клика
+    deleteCard.addEventListener("click", callback)
+  );
+}
+
+render();
