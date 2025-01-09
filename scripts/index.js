61b7cb455c85ca3161fffdc95b5a44d9b753bb23
Даша <dpodkina@mail.ru>
2025-01-09 10:43:07 +0400

    Исправление ошибок после 4 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Объявили переменную, куда выведем готовые карточки
 const placesList = document.querySelector(".places__list");
 
-function renderCard({ name, link }) {
+function renderCard({ name, link }, deleteCard) {
   //Скопировали шаблон
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate
@@ -10,18 +10,15 @@ function renderCard({ name, link }) {
   //Установили значения вложенных элементов
   cardElement.querySelector(".card__title").textContent = name;
   cardElement.querySelector(".card__image").src = link;
-  cardElement.setAttribute("alt", name);
+  cardElement.querySelector(".card__image").setAttribute("alt", name);
   // обработчик клика, по которому будет вызван переданный в аргументах колбэк (функция).
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", function deleteCard() {
+    const cards = cardElement.closest("li");
+    cards.remove();
+  });
   //возвращаем созданную карточку
   return cardElement;
 }
 
-//Функция удаления
-function deleteCard() {
-  const cards = this.parentElement;
-  cards.remove();
-}
-
-initialCards.map(renderCard, deleteCard).forEach((el) => placesList.append(el));
+initialCards.map(renderCard).forEach((el) => placesList.append(el));
