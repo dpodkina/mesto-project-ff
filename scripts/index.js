f3ef87432190b5c697f256a080415104fd3362fa
Даша <dpodkina@mail.ru>
2025-01-27 21:05:20 +0400

    Исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,29 @@
 // Объявили переменную, куда выведем готовые карточки
 const placesList = document.querySelector(".places__list");
 
-function renderCard({ name, link }, deleteCard) {
-  //Скопировали шаблон
+function renderCard({ name, link }) {
+  // Скопировали шаблон
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
-  //Установили значения вложенных элементов
+  // Установили значения вложенных элементов
   cardElement.querySelector(".card__title").textContent = name;
   cardElement.querySelector(".card__image").src = link;
   cardElement.querySelector(".card__image").setAttribute("alt", name);
-  // обработчик клика, по которому будет вызван переданный в аргументах колбэк (функция).
+  // Обработчик клика, по которому будет вызван переданный в аргументах колбэк (функция).
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", function deleteCard() {
-    const cards = cardElement.closest("li");
-    cards.remove();
-  });
-  //возвращаем созданную карточку
+  deleteButton.addEventListener("click", deleteCard);
   return cardElement;
 }
 
-initialCards.map(renderCard).forEach((el) => placesList.append(el));
+// Функция удаления
+function deleteCard() {
+  const cards = this.parentElement;
+  cards.remove();
+}
+
+// Массив с данными
+initialCards
+  .map(renderCard) // Преобразуем в массив элементов
+  .forEach((el) => placesList.append(el)); // каждый элемент добавляем в контейнер
